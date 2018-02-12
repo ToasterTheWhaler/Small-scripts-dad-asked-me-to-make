f8cabb495377c461a1c6d85f12892e8f5b87388d
ToasterTheWhaler <36408386+ToasterTheWhaler@users.noreply.github.com>
2018-02-12 13:36:09 +0100

    Update Version 2.py

--- Version 2.py
+++ Version 2.py
@@ -1,11 +1,11 @@
-inputlist = []
-listlength = 1
-maxLengthList = int(input("Ilosc kwadratow"))
-while len(inputlist) < maxLengthList:
-    item = int(input("Ilosc plemnikow w kwadracie NR " + str(listlength) + ""))
-    inputlist.append(item)
-    listlength += 1
-print("Wartosci kwadratow" + str(inputlist) + "")
+input_list = []
+list_length = 1
+max_length_list = int(input("Ilosc kwadratow"))
+while len(input_list) < max_length_list:
+    item = int(input("Ilosc plemnikow w kwadracie NR " + str(list_length) + ""))
+    input_list.append(item)
+    list_length += 1
+print("Wartosci kwadratow" + str(input_list) + "")
 S = int(input("Rozcieczenie"))
 Ml = int(input("Ilosc Ml"))
-print('%.2E' % (((((sum(inputlist)/maxLengthList*S)*250)/maxLengthList)*1000)*Ml))
+print('%.2E' % (((((sum(input_list)/max_length_list*S)*250)/max_length_list)*1000)*Ml))
